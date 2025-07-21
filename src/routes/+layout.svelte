4cb28cfea26846be63aebede27df4c3709201676
George Hotten <github@ghott.me>
2025-07-21 17:20:53 +0100

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import '../gweb.scss';
+</script>
+
+<main class="gweb-container">
+    <slot />
+</main>
+
+<footer class="gweb-footer footer-sticky">
+    <div class="content footer-text has-text-centered">
+        <div class="gweb-text-layout">
+            <p style="font-size: 15px;"><b>© 2025 Hotten Technology Group</b></p>
+            <p style="font-size: 15px;" class="gweb-link-white">
+                <a href="https://github.com/gxorge/">github</a> &middot;
+                <a href="https://hotten.uk">hotten.uk</a> &middot;
+                <a href="https://hotten.cloud">hotten.cloud</a> &middot;
+                <a href="/legal">legal</a>
+            </p>
+        </div>
+        <div class="footer-colours">
+            <div class="footer-colour" id="aston"></div>
+            <div class="footer-colour" id="solihull"></div>
+            <div class="footer-colour" id="george"></div>
+            <div class="footer-colour" id="hotten"></div>
+        </div>
+    </div>
+</footer>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import HeroTitle from '$lib/HeroTitle.svelte';
+    import BackBreadcrum from '$lib/BackBreadcrum.svelte';
+</script>    
+
+<svelte:head>
+    <title>Hotten Technology Group</title>
+</svelte:head>
+
+<HeroTitle title="Hotten Technology Group"/>
+<section class="content">
+    <div class="gweb-text-layout">
+        <p>
+            Hotten Technology is an organisation providing computer and IT services in the UK. We currently operate two parent services:
+        </p>
+
+        <h2>Hotten UK</h2>
+        <p class="gweb-link-black">
+            <a href="https://hotten.uk">Hotten UK</a> provides personal website hosting and design. Examples include <a href="https://george.hotten.uk">George's personal website</a> and his education related publishings.
+        </p>    
+
+        <h2>Hotten Cloud</h2>
+        <p class="gweb-link-black">
+            <a href="https://hotten.cloud">Hotten Cloud</a> provides the infrastructure used to host Hotten UK, alongside many services used by family and friends.
+        </p>    
+    </div>
+</section>
